a41000ae7687ebce10a0655d6fe5aae8838e2535
rafael <rafaondjango@gmail.com>
2024-02-23 19:34:41 -0300

    Add project files.

---
+++ TestTechNation/Program.cs
@@ -0,0 +1,39 @@
+﻿int num;
+
+do
+{
+    Console.Write("Digite um número (0 para sair): ");
+    string? numTemp = Console.ReadLine();
+
+    if (!int.TryParse(numTemp!, out num))
+    {
+        Console.WriteLine("Por favor, digite um número válido!");
+        continue;
+    }
+
+    if (num <= 0)
+    {
+        break;
+    }
+
+    bool isPrime = true;
+
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime && num > 1)
+    {
+        Console.WriteLine($"O número {num} é primo!");
+    }
+    else
+    {
+        Console.WriteLine($"O número {num} não é primo!");
+    }
+
+} while (true);
